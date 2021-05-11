02c83aaafa71ae2e692016810b1c421ad7b51b12
Kyerstin O'Neal <kcrocker1@cnm.edu>
2021-05-11 15:39:13 -0600

    created circular image slider directory; scripting html

--- horizontal-parallax-slider/app.js
+++ horizontal-parallax-slider/app.js
@@ -1,18 +1,21 @@
-let images = [...document.querySelectorAll('.img')];
+// variables
+let images = [...document.querySelectorAll('.img')]; // spread operator in an array
 let slider = document.querySelector('.slider');
 let sliderWidth;
 let imageWidth;
 let current = 0;
 let target = 0;
-let ease = .05;
+let ease = .05; // ease variable
 
 window.addEventListener('resize', init);
 
+// forEach function passes in the index for each image; using +1 as starting on 1 not 0
 images.forEach((img, idx) => {
-    img.style.backgroundImage = `url(./images/${idx+1}.jpeg)`
+    img.style.backgroundImage = `url(./images/${idx+1}.jpg)`
 })
 
-function lerp(start, end, t){
+// linear interpolation function: establish start & end position & t easing variable works out the % between the start & end
+function lerp(start, end, t){ // t variable is the ease
     return start * (1-t) + end * t;
 }
 
@@ -28,7 +31,7 @@ function init(){
 
 function animate(){
     current = parseFloat(lerp(current, target, ease)).toFixed(2);
-    target = window.scrollY;
+    target = window.scrollY; // amount of pixels we've scrolled down by
     setTransform(slider, `translateX(-${current}px)`)
     animateImages();
     requestAnimationFrame(animate);
@@ -40,7 +43,7 @@ function animateImages(){
 
     images.forEach((image, idx) => {
         intersectionRatioValue = ratio - (idx * 0.7);
-        setTransform(image, `translateX(${intersectionRatioValue * 70})px`)
+        setTransform(image, `translateX(${intersectionRatioValue * 70}px)`)
     })
 }
 
